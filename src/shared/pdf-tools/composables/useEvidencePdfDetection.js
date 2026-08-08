5df36fa7a21c4a6844bf6115ff4982345bc92b09
muxiaoxiii <oonlyxin@outlook.com>
2026-08-09 04:26:25 +0800

    MDG-023: 清理死代码 DETECTION_SCAN_MAX_PAGES

--- src/shared/pdf-tools/composables/useEvidencePdfDetection.js
+++ src/shared/pdf-tools/composables/useEvidencePdfDetection.js
@@ -4,7 +4,6 @@ import { emitOperationEvent, emitOperationUpdate } from '../../../core/tauriBrid
 import { candidateTargetRange } from './useEvidencePdfSession.js'
 import { candidateIdentity, detectedElementFromCandidate, mergeExistingElements } from './existingPdfElements.js'
 
-const DETECTION_SCAN_MAX_PAGES = 0
 const ROMAN_PAGE_SCORE_PENALTY = -0.25
 
 export function headerFooterDetectionZoneMm(value) {
